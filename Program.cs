5aa4afe9a0975ef3413e738729a996718defc64d
JavierSalazar19 <javierasc2002@gmail.com>
2023-11-22 08:18:08 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using PracticaFacade.Subsystem;
+using PracticaFacade.Facade;
+
+ContactSystem subsystem= new ContactSystem();
+
+ContactFacade facade = new ContactFacade(subsystem);
+
+Console.WriteLine(facade.create().ToString());
---
+++ Subsystem/ContactSystem.cs
@@ -0,0 +1,32 @@
+﻿using PracticaFacade.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaFacade.Subsystem
+{
+    public class ContactSystem
+    {
+        public Contact CreateContact()
+        {
+            Console.WriteLine("introduzca el nombre de usuario");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("introduzca el email");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("introduzca el numero telefonico");
+            string phone = Console.ReadLine();
+
+            Console.WriteLine("introduzca la contra");
+            string password = Console.ReadLine();
+
+            Contact contacto1 = new Contact(name, email, phone, password);
+
+            return contacto1;
+        }
+
+    }
+}
